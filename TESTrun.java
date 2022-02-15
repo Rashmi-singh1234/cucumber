006ed66c27e4d6e73b788b7f34b77ea4e1793ef9
Rashmi-singh1234 <99351314+Rashmi-singh1234@users.noreply.github.com>
2022-02-15 22:46:46 +0530

    Add files via upload

---
+++ TESTrun.java
@@ -0,0 +1,12 @@
+package stepdefinition;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/featuress",glue= {"stepdefinition"})
+public class TESTrun {
+
+}
